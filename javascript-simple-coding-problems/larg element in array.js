a57b285016cb8d5db3eb421cea6fce1580f97491
Your Name <you@example.com>
2021-12-08 22:09:23 +0600

    resolve

--- javascript-simple-coding-problems/larg element in array.js
+++ javascript-simple-coding-problems/larg element in array.js
@@ -1,28 +1,28 @@
-// find out largest element in array.. ues function and in use for loop
-function largElement(numbers) {
-    let maxValue = numbers[0]
-    for (let i = 0; i < numbers.length; i++){
-        const element = numbers[i]
-        if (element > maxValue) {
-            maxValue = element;
-        }
-        else {
-            maxValue = maxValue;
-        }
-    }
-    return maxValue;
-}
-const myArray = [23, 45, 67, 178, 45, 76, 89, 78, 99, 100];
-const result = largElement(myArray);
-console.log(result);
+// // find out largest element in array.. ues function and in use for loop
+// function largElement(numbers) {
+//     let maxValue = numbers[0]
+//     for (let i = 0; i < numbers.length; i++) {
+//         const element = numbers[i]
+//         if (element > maxValue) {
+//             maxValue = element;
+//         }
+//         else {
+//             maxValue = maxValue;
+//         }
+//     }
+//     return maxValue;
+// }
+// const myArray = [23, 45, 67, 178, 45, 76, 89, 78, 99, 100];
+// const result = largElement(myArray);
+// console.log(result);
 
 
 // find out largest element in array.. ues function and in use while loop
 function myFunck(price) {
     let i = 0;
-    maxVAlue = price[0];
+    let maxVAlue = price[0];
     while (i < price.length) {
-        element = price[i];
+        const element = price[i];
         if (element > maxVAlue) {
             maxVAlue = element;
         }
